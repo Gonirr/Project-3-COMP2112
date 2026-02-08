74466d47cc6c9105429918067a119b8d99d73912
gokche <gokcedinar.d@gmail.com>
2026-02-08 21:28:57 +0300

    string check done

--- src/raceofrandom/GameFrame.java
+++ src/raceofrandom/GameFrame.java
@@ -1,49 +1,64 @@
 package raceofrandom;
 import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GameFrame extends JFrame{
     
-    JFrame frame;
     CardLayout card;
     GamePanel gamepanel;
     JPanel ınputpanel;
     
     GameFrame(){
-        frame=new JFrame("Race Of Random");
         card=new CardLayout();
+        this.setLayout(card);
         gamepanel=new GamePanel();
         ınputpanel=new JPanel();
         JTextField text=new JTextField(10);
         JButton button=new JButton("submit");
+        JLabel label=new JLabel("Please enter how many cars you want in the race(0-15):");
+        
         
         button.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
                 String s=text.getText();
-                System.out.println("s");
+                if(stringCheck(s)){
+                    card.next(getContentPane());
+                }
             }
         });
-        ınputpanel.setLayout(new FlowLayout());
+        ınputpanel.add(label);
         ınputpanel.add(text);
         ınputpanel.add(button);
-        frame.add(ınputpanel);
-        frame.add(gamepanel);
+        this.add(ınputpanel);
+        this.add(gamepanel);
         
-        frame.setLayout(card);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(new Dimension(500,300));
         
-        frame.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //this.setResizable(false);
+        this.setSize(new Dimension(500,500));
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
         
+    }
+    
+    public boolean stringCheck(String s){
+        try{
+            int is=Integer.parseInt(s);
+            if(is<=15){
+                return true;
+            }
+            return false;
+        }
+        catch(NumberFormatException e){return false;}
     }
     
 }
--- src/raceofrandom/GamePanel.java
+++ src/raceofrandom/GamePanel.java
@@ -1,11 +1,7 @@
 package raceofrandom;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
+import Graph.GraphList;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-    
+    GraphList racetrack;
 }
